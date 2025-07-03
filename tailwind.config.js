7a2e797ae19a1e3df0e68c49385a7e7531775bd6
joeaurick <joeaurick@JOE-RICK.local>
2025-07-04 05:05:49 +0700

    fix: move app to src and fix config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     container: {
@@ -12,9 +12,7 @@ module.exports = {
         '2xl': '1440px',
       },
     },
-    extend: {
-      // bisa tambah warna, font, dll di sini
-    },
+    extend: {},
   },
   plugins: [],
 }
