0dd7d3189e8dec44d28cc1977ca88be6961f0622
joeaurick <joeaurick@JOE-RICK.local>
2025-07-04 05:33:37 +0700

    fix: move app folder to root and ensure page.tsx routing works

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: false, // jangan pakai true kalau tidak export statis
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
